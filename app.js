d17715b4fb35a4c01e58da653e9fd26dc0354ddf
saketh bachu <saketh7000@gmail.com>
2020-02-07 23:22:21 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,24 @@
+const navSlide = () => {
+  const burger = document.querySelector('.burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-link li');
+
+  burger.addEventListener('click', () => {
+
+      nav.classList.toggle('nav-active');
+
+    //Animate navLinks
+      navLinks.forEach((link, index) => {
+          if (link.style.animation) {
+              link.style.animation = '';
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 5 + 0.5}s`;
+
+         }
+      });
+      //animation
+      burger.classList.toggle('toggle');
+   });
+}
+
+navSlide();
